9f67fd45e8c12757cdb403b51631fc0422df73fc
adapasrinivas01 <adapasrinivas2004@gmail.com>
2025-08-09 22:43:54 +0530

    Create findingsquareroots.cpp

---
+++ findingsquareroots.cpp
@@ -0,0 +1,15 @@
+here#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	    int a;
+	    cin>>a;
+	    cout<<(int)sqrt(a)<<endl;
+	}
+
+}
